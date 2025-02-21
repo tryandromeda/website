5b81dd42649fc404a01da8ae3f0f43a36dd86afd
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2025-02-20 19:45:08 -0500

    feat: meta tags

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,6 +6,12 @@ export default function App({ Component }: PageProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Andromeda</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://tryandromeda.dev/" />
+        <meta property="og:title" content="Andromeda" />
+        <meta property="og:description" content="Andromeda - Rust-powered JavaScript and TypeScript runtime" />
+        <meta name="description" content="Andromeda - Rust-powered JavaScript and TypeScript runtime" />
+        <meta name="keywords" content="Andromeda, Rust, JavaScript, TypeScript, runtime" />
         <link rel="stylesheet" href="/styles.css" />
         <link rel="stylesheet" href="/prism.css" />
         <link rel="icon" href="/logo.svg" />
