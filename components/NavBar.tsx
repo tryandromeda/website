5dd77202111e254f4e4c945e75fc0f02389a3c3c
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2025-08-20 22:42:15 -0400

    feat: std -> standard library

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -43,7 +43,7 @@ export default function NavBar() {
                 href="/std"
                 class="text-subtext1 hover:text-text transition-all duration-300 font-medium text-sm relative group"
               >
-                Std
+                Standard Library
                 <span class="absolute -bottom-1 left-0 w-0 h-0.5 bg-yellow transition-all duration-300 group-hover:w-full">
                 </span>
               </a>
